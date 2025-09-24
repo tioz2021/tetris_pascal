efaa622eb5b135b0c23c969925f55dc73d58e90d
tios23 <jenya25life@gmail.com>
2025-09-24 21:54:33 +0300

    update

---
+++ get_key.pas
@@ -0,0 +1,25 @@
+unit get_key;
+
+interface
+procedure GetKey(var code: integer);
+
+implementation
+uses crt;
+
+procedure GetKey(var code: integer);
+var
+    c: char;
+begin
+    c := ReadKey;
+    if c = #0 then
+    begin
+        c := ReadKey;
+        code := -ord(c)
+    end
+    else
+    begin
+        code := ord(c)
+    end
+end;
+
+end.
--- main.pas
+++ main.pas
@@ -1,4 +1,70 @@
 program main;
-begin
+uses crt, get_key;
 
+const
+    figureBg = '#';
+
+procedure PrintFigure(figureId: integer; curX, curY: integer);
+var
+    i: integer;
+begin
+    for i := 1 to 9 do
+    begin
+        write(figureBg);
+        {delay(100);}
+        curX := curX + 1;
+        GotoXY(curX, curY);
+    end
+end;
+
+procedure MainProcedure();
+var
+    x, y: integer;
+    curX, curY: integer;
+begin
+    x := ScreenWidth div 2;
+    y := ScreenHeight div 2;
+    curX := x;
+    curY := y;
+    GotoXY(x, y);
+
+    PrintFigure(1, curX, curY);
+end;
+
+var
+    saveTextAttr: integer;
+    x, y: integer;
+    keyCode: integer;
+begin
+    { init param }
+    clrscr;
+    saveTextAttr := TextAttr;
+
+    { main procedure }
+    while true do
+    begin
+        if KeyPressed then
+        begin
+            GetKey(keyCode);
+            case keyCode of
+                32: 
+                begin
+                    GotoXY(1, 1);
+                    clrscr;
+                    halt(1)
+                end;
+                27: 
+                begin
+                    GotoXY(1, 1);
+                    clrscr;
+                    halt(1)
+                end
+            end
+        end;
+        MainProcedure()
+    end;
+
+    { exit program }
+    TextAttr := saveTextAttr;
+    clrscr
 end.
